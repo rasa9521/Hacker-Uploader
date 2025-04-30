bb96719a12da796f938599fcaf3b1ce605941cff
rsrawatofficial <rsrasasingh@gmail.com>
2025-04-30 08:16:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ api_id =  27900743
 api_hash  = 'ebb06ea8d41420e60b29140dcee902fc'
 bot_token =  ''
 OWNER =  7804396225
-OWNER_USERNAME = 'mrkrsrawat'
+OWNER_USERNAME = ''
 LOG =  -1002682536048
 ADMINS = [OWNER, 7804396225]
